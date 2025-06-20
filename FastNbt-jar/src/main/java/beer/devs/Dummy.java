c23a495e7fa088d3ca00b665e05409bb2358d57a
LoneDev <27242001+LoneDev6@users.noreply.github.com>
2025-06-20 16:31:21 +0200

    Build improvements
    - Avoid publishing useless packages on Maven Central
    - Do not report javadocs warnings

--- FastNbt-jar/src/main/java/beer/devs/Dummy.java
+++ FastNbt-jar/src/main/java/beer/devs/Dummy.java
@@ -1,3 +1,7 @@
 package beer.devs;
 
+/**
+ * This is a dummy class to ensure that the package structure is valid.
+ * I do not remember exactly why I added that nor if it's still needed, but I will keep it here for now.
+ */
 public class Dummy {}
