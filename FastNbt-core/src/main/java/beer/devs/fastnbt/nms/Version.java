7ef20cadefcbe94392bb5abe640afb9308e5e19f
LoneDev <27242001+LoneDev6@users.noreply.github.com>
2026-06-19 16:47:36 +0200

    1.4.20
    - Support Minecraft 26.2
    - Refactoring of Version class, might require some adjustments on your side as some fields were removed
    - Implemented Version#getPackVersion() method

--- FastNbt-core/src/main/java/beer/devs/fastnbt/nms/Version.java
+++ FastNbt-core/src/main/java/beer/devs/fastnbt/nms/Version.java
@@ -10,35 +10,36 @@ import org.bukkit.Bukkit;
 @SuppressWarnings({"unused"})
 public enum Version
 {
-    UNKNOWN(Integer.MAX_VALUE, Integer.MAX_VALUE, ""),
+    UNKNOWN(Integer.MAX_VALUE, ""),
     @Deprecated
-    v1_15_R1(0, 578, "1.15.2"),
+    v1_15_R1(578, "1.15.2"),
     @Deprecated
-    v1_16_R3(10, 754, "1.16.5"),
-    v1_17_R1(20, 756, "1.17.1"),
-    v1_18_R1(30, 757, "1.18.1"),
-    v1_18_R2(40, 758, "1.18.2"),
-    v1_19_R1(50, 760, "1.19.2"),
-    v1_19_R2(60, 761, "1.19.3"),
-    v1_19_R3(70, 762, "1.19.4"),
-    v1_20_R1(80, 763, "1.20.1"),
-    v1_20_R2(90, 764, "1.20.2"),
-    v1_20_R3(100, 765, "1.20.3"),
-    v1_20_4(110, 765, "1.20.4"), // v1_20_R3
-    v1_20_5(120, 766, "1.20.5"), // v1_20_R4
-    v1_20_6(130, 766, "1.20.6"), // v1_20_R4
-    v1_21(140, 767, "1.21"),
-    v1_21_1(141, 767, "1.21.1"),
-    v1_21_3(143, 768, "1.21.3"),
-    v1_21_4(144, 769, "1.21.4"),
-    v1_21_5(145, 770, "1.21.5"),
-    v1_21_6(146, 771, "1.21.6"),
-    v1_21_7(147, 772, "1.21.7"),
-    v1_21_8(148, 772, "1.21.8"),
-    v1_21_10(149, 773, "1.21.10"),
-    v1_21_11(150, 774, "1.21.11"),
-    v26_1_1(162, 775, "26.1.1"),
-    v26_1_2(163, 775, "26.1.2")
+    v1_16_R3(754, 6, "1.16.5"),
+    v1_17_R1(756, 7, "1.17.1"),
+    v1_18_R1(757, 8, "1.18.1"),
+    v1_18_R2(758, 8, "1.18.2"),
+    v1_19_R1(760, 11, "1.19.2"),
+    v1_19_R2(761, 12, "1.19.3"),
+    v1_19_R3(762, 13, "1.19.4"),
+    v1_20_R1(763, 14, "1.20.1"),
+    v1_20_R2(764, 16, "1.20.2"),
+    v1_20_R3(765, 22, "1.20.3"),
+    v1_20_4(765, 22, "1.20.4"), // v1_20_R3
+    v1_20_5(766, 32, "1.20.5"), // v1_20_R4
+    v1_20_6(766, 32, "1.20.6"), // v1_20_R4
+    v1_21(767, 34, "1.21"),
+    v1_21_1(767, 34, "1.21.1"),
+    v1_21_3(768, 42, "1.21.3"),
+    v1_21_4(769, 46, "1.21.4"),
+    v1_21_5(770, 55, "1.21.5"),
+    v1_21_6(771, 63, "1.21.6"),
+    v1_21_7(772, 64, "1.21.7"),
+    v1_21_8(772, 64, "1.21.8"),
+    v1_21_10(773, 69, "1.21.10"),
+    v1_21_11(774, 75, "1.21.11"),
+    v26_1_1(775, 75, "26.1.1"),
+    v26_1_2(775, 84, "26.1.2"),
+    v26_2(776, 84, "26.2")
     ;
 
     private static Version version;
@@ -46,18 +47,60 @@ public enum Version
     /**
      * Protocol ID.
      */
-    public final int id;
-    // Used internally by the plugin to identify the version chronologically,
-    // because sometimes the Mojang protocol version doesn't increase.
-    @Deprecated // Named protocol by mistake a long time ago. `id` and `protocol` are swapped.
-    public final int protocol;
-    public final String name;
+    private final int protocol;
+    private final String name;
+    private final int packVersion;
 
-    Version(int protocol, int id, String name)
+    Version(int protocol, String name)
     {
-        this.id = id;
+        this(protocol, 1, name);
+    }
+
+    Version(int protocol, int packVersion, String name)
+    {
         this.protocol = protocol;
         this.name = name;
+        this.packVersion = packVersion;
+    }
+
+    /**
+     * Gets the ordinal value of this version.
+     *
+     * @return the ordinal value
+     */
+    public int getOrdinal()
+    {
+        return ordinal();
+    }
+
+    /**
+     * Gets the protocol version number.
+     *
+     * @return the protocol version number
+     */
+    public int getProtocol()
+    {
+        return protocol;
+    }
+
+    /**
+     * Gets the version name.
+     *
+     * @return the version name
+     */
+    public String getName()
+    {
+        return name;
+    }
+
+    /**
+     * Gets the pack version number.
+     *
+     * @return the pack version number
+     */
+    public int getPackVersion()
+    {
+        return packVersion;
     }
 
     public static Version byName(String name)
@@ -78,7 +121,7 @@ public enum Version
      */
     public static boolean isAtLeast(Version version)
     {
-        return get().protocol >= version.protocol;
+        return get().ordinal() >= version.ordinal();
     }
 
     /**
@@ -89,7 +132,7 @@ public enum Version
      */
     public static boolean isNewerThan(Version version)
     {
-        return get().protocol > version.protocol;
+        return get().ordinal() > version.ordinal();
     }
 
     /**
@@ -100,7 +143,7 @@ public enum Version
      */
     public static boolean isOlderThan(Version version)
     {
-        return get().protocol < version.protocol;
+        return get().ordinal() < version.ordinal();
     }
 
     public static Version get()
